a1ec41f07828cca952f2656352efb51ba81fb808
giovanemachado <giovanemachado3@gmail.com>
2023-08-18 22:42:00 -0300

    setup

---
+++ src/utils/scripts/Globals.gd
@@ -0,0 +1,24 @@
+extends Node
+
+const GROUPS = {
+	ENEMY = 'enemy',
+	PLAYER = 'player',
+	FRIEND = 'friend',
+	COLLECTABLE = 'collectable'
+}
+
+const ITEM_NAMES = {
+	FOOD = 'food'
+}
+
+#enum ExampleEnum {A, B}
+#
+#const EXAMPLE_CONST = 'const value'
+#
+#const EXAMPLE_CONST_OBJECT = { 
+#		PROPERTY1 = 'preperty 1', 
+#		PROPERTY2 = 'property 2',
+#}
+
+# To use:
+# Globals.EXAMPLE_CONST
---
+++ src/utils/scripts/SignalBus.gd
@@ -0,0 +1,12 @@
+extends Node
+
+signal game_over
+signal player_ride
+signal player_unride
+
+
+# To connect:
+# SignalBus.game_over.connect(_on_game_over)
+
+# To emit:
+# SignalBus.emit_signal("game_over")
